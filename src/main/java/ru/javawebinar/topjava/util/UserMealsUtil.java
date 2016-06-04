968829a6087397d9a897d9f841ead15624f3b4ed
ROleP <>
2016-06-04 19:09:07 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * GKislin
@@ -23,13 +23,28 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
+
+        List<UserMealWithExceed> filteredMealsWithExceeded = getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        filteredMealsWithExceeded.forEach(System.out::println);
     }
 
     public static List<UserMealWithExceed>  getFilteredMealsWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO
-        return null;
+        //Creating list for filtered result meals with exceed
+        List<UserMealWithExceed> result = new ArrayList<>();
+        //map to calculate sum of total callories per day, key = day, value = callories
+        Map<LocalDate, Integer> dailyCallories = new HashMap<>();
+        //calculating total callories per day - for each meal adding its calls to this days total calls
+        //complexity of this iteration is O(N)
+        mealList.stream().forEach((e) ->
+            dailyCallories.put(e.getDateTime().toLocalDate(), e.getCalories() + dailyCallories.getOrDefault(e.getDateTime().toLocalDate(), 0)));
+        //Now filtering meals by specified time and creating new meal with exceed and adding it to result list
+        //with proper exceed parameter
+        //Complexity of this iteration O(N)
+        mealList.stream().filter((e) -> TimeUtil.isBetween(e.getDateTime().toLocalTime(), startTime, endTime)
+        ).forEach((e) -> {
+            result.add(new UserMealWithExceed(e.getDateTime(), e.getDescription(), e.getCalories(), dailyCallories.get(e.getDateTime().toLocalDate()) > caloriesPerDay));
+        });
+        //Total complexity O(N)
+        return result;
     }
 }
